d2a23716c186681bc6dc641007c4ed46eea6eb6b
Alex Azevedo <alex.gadita@gmail.com>
2024-07-24 19:59:52 -0300

    criando o projeto para exemplo de tipos variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá galera");
+        int numeroUm = 1;
+        int numeroDois = 2;
+
+        boolean simNao = numeroUm == numeroDois;
+        
+        System.out.println("numero Um é igual a numeroDois: " + simNao);
     }
 }
